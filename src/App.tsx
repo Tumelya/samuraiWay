e6b81908ebf0dfa1a043663e48a7ef52bd6e95a1
Mariya Tumelya <mtumelia@gmail.com>
2022-07-13 13:48:23 +0300

    feat: add new components and rouring

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,30 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {Footer} from "./components/Footer/Footer";
 import {Navbar} from "./components/Navbar/Navbar";
+import {BrowserRouter, Route} from "react-router-dom";
 import {Profile} from "./components/Profile/Profile";
+import {Dialogs} from "./components/Dialogs/Dialogs";
+import {News} from "./components/News/News";
+import {Music} from "./components/Music/Music";
+import {Settings} from "./components/Settings/Settings";
 
 
 function App() {
     return (
-        <div className="App">
-            <Header/>
-            <Navbar/>
-            <div className="content">
-                <Profile/>
+        <BrowserRouter>
+            <div className="App">
+                <Header/>
+                <Navbar/>
+                <div className="content">
+                    <Route path="/profile" component={Profile}/>
+                    <Route path="/dialogs" component={Dialogs}/>
+                    <Route path="/news" component={News}/>
+                    <Route path="/music" component={Music}/>
+                    <Route path="/settings" component={Settings}/>
+                </div>
+                <Footer/>
             </div>
-            <Footer/>
-        </div>
+        </BrowserRouter>
     )
 }
 
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,7 +4,7 @@ import s from "./Dialogs.module.css";
 export const Dialogs = () => {
     return (
         <div>
-
+            Dialogs here
         </div>
     )
 }
